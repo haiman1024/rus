d1590be55a6fe08fe16ff71dd2e28e274bc9610a
haiman1024 <alexredskaber@gmail.com>
2025-08-10 19:15:38 +0800

    Refactors lexer to use BufReader
    Updates the lexer to utilize `BufReader` for reading input,
    allowing for more efficient line-by-line processing.
    
    This change simplifies the lexer implementation by removing
    the custom `BufCharReader` and directly using `BufReader`
    to handle reading and buffering of the input stream. It also
    correctly updates location information, tracking line and
    column numbers as the input is processed. It also adds basic
    token parsing for the '+' character.

--- src/lex.rs
+++ src/lex.rs
@@ -1,33 +1,67 @@
-use std::iter::Iterator;
+use std::io::{BufRead, BufReader, Read};
+use std::iter::IntoIterator;
+use std::vec::IntoIter;
 
-use super::data::{Error, Locatable, Location, Token, TokenType};
+use super::data::{Error, Location, Token, TokenType};
 
-pub type Lexer<'a> = Locatable<'a, &'a mut dyn Iterator<Item = char>>;
+pub struct Lexer<'a, R: Read> {
+    location: Location<'a>,
+    reader: BufReader<R>,
+    iterator: IntoIter<char>,
+}
 
-impl<'a> Lexer<'a> {
-    pub fn new(filename: &'a str, stream: &'a mut dyn Iterator<Item = char>) -> Lexer<'a> {
+impl<'a, R: Read> Lexer<'a, R> {
+    pub fn new(filename: &'a str, stream: BufReader<R>) -> Lexer<'a, R> {
         Lexer {
             location: Location {
                 line: 1,
-                column: 1,
+                column: 0,
                 file: filename,
             },
-            data: stream,
+            reader: stream,
+            iterator: Vec::new().into_iter(),
+        }
+    }
+
+    fn next_char(&mut self) -> Option<char> {
+        match self.iterator.next() {
+            Some(c) => Some(c),
+            None => {
+                let mut buf: String = String::new();
+                return match self.reader.read_line(&mut buf) {
+                    Ok(_) => {
+                        self.location.line += 1;
+                        self.location.column = 1;
+                        self.iterator = buf.chars().collect::<Vec<_>>().into_iter();
+                        self.iterator.next()
+                    }
+                    Err(_) => None,
+                };
+            }
         }
     }
 }
 
-impl<'a> Iterator for Lexer<'a> {
+impl<'a, R: Read> Iterator for Lexer<'a, R> {
+    // option: whether the stream is exhausted
+    // result: whether the next lexeme is an error
     type Item = Result<Token<'a>, Error<'a>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let token = match self.data.next() {
-            Some(_c) => TokenType::Plus,
-            None => return None,
-        };
-        Some(Ok(Token {
-            location: self.location.clone(),
-            data: token,
-        }))
+        self.next_char().and_then(|c| {
+            let location = self.location.clone();
+            let result = match c {
+                '+' => Ok(Token {
+                    location: location,
+                    data: TokenType::Plus,
+                }),
+                _ => Err(Error {
+                    location,
+                    data: String::from("Unknown token"),
+                }),
+            };
+            self.location.column += 1;
+            Some(result)
+        })
     }
 }
